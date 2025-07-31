a16fa80a1f305d5f832cb604584f03b33fa04cfa
jalvaradod10 <jalvaradod@uninorte.edu.co>
2025-07-31 21:44:46 +0000

    se pregunto al usuario la cantidad de numeros de la secuencia que desea

---
+++ src/fibonacci.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cuantos numeros de Fibonacci desea: ");
+        int n=sc.nextInt();
+        int [] fibonacci = new int[n];
+        for (int i = 1; i < n; i++) {
+            fibonacci[]= fibonacci[]
+        }
+    }
+}
